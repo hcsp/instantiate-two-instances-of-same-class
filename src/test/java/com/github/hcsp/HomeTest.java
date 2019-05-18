6b00b6df16e207d25181dcd0c1b29f28d1f92896
Bo Zhang <bo@gradle.com>
2019-05-18 21:11:07 +0800

    Fix failed tests

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -4,11 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class MainTest {
+public class HomeTest {
     @Test
     public void testNewCats() {
         Main.createTwoCats();
-        assertEquals("cat(black)", Main.black.toString().toLowerCase());
-        assertEquals("cat(white)", Main.white.toString().toLowerCase());
+        assertEquals("cat(black)", Home.black.toString().toLowerCase());
+        assertEquals("cat(white)", Home.white.toString().toLowerCase());
     }
 }
