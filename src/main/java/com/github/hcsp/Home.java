ee47729b6ebc40df6193456bb93eff0159461149
Administrator <908380434@qq.com>
2019-06-04 22:50:24 +0800

    创建两个名字为"Black"和"White"的Cat实例，分别赋值给"black"和"white"静态变量

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -16,9 +16,9 @@ public class Home {
         // Create two new Cats named "Black" and "White"
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个名字为"Black"和"White"的Cat实例，分别赋值给"black"和"white"静态变量
-        Cat Black = new Cat();
-        Cat White = new Cat();
-        black = Black;
-        white = White;
+        white = new Cat();
+        white.name = "White";
+        black = new Cat();
+        black.name = "Black";
     }
 }
