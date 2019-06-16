59e3c5d705e5369dfb0ecbbfa01c68374c2c9478
Ethan Wang <1527185002@qq.com>
2019-06-16 20:28:14 +0800

    创建同一个类的多个对象。

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -3,6 +3,9 @@ package com.github.hcsp.pet;
 public class Cat {
     public String name;
 
+    public Cat(String name){
+        this.name = name;
+    }
     @Override
     public String toString() {
         return "Cat(" + name + ")";
