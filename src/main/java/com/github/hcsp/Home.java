47a766bc3f3d1e5eb73e9b01d788bb87df69a478
dflk <904885508@qq.com>
2019-06-21 21:48:39 +0800

    '创建两个名字为Black和White的Cat实例，分别赋值给black和white静态变量'

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -1,6 +1,7 @@
 package com.github.hcsp;
 
 import com.github.hcsp.pet.Cat;
+import com.sun.deploy.util.BlackList;
 
 public class Home {
     public static Cat black;
@@ -16,5 +17,9 @@ public class Home {
         // Create two new Cats named "Black" and "White"
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个名字为"Black"和"White"的Cat实例，分别赋值给"black"和"white"静态变量
+        Cat Black = new Cat();
+        Cat White = new Cat();
+        black = Black;
+        white = White;
     }
 }
