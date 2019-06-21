fc2139c8e95a45a54abaf6171ac5addf89710711
东方来客 <39690418+westee@users.noreply.github.com>
2019-06-21 22:06:11 +0800

    这次行了 (#34)
    这次行了 (#34)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -16,5 +16,11 @@ public class Home {
         // Create two new Cats named "Black" and "White"
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个名字为"Black"和"White"的Cat实例，分别赋值给"black"和"white"静态变量
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        cat1.name = "Black";
+        cat2.name = "White";
+        black = cat1;
+        white = cat2;
     }
 }
