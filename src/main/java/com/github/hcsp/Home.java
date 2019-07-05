8508696882d4b6b17893b4d4ce1f9e03dda346fe
Zhang Xin <zhangxindev@gmail.com>
2019-07-05 17:52:16 +0800

    创建相同类的不同实例

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -16,5 +16,10 @@ public class Home {
         // Create two new Cats named "Black" and "White"
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个名字为"Black"和"White"的Cat实例，分别赋值给"black"和"white"静态变量
+        Cat black = new Cat();
+        Cat white = new Cat();
+        Black.name = "black";
+        White.name = "white";
+
     }
 }
