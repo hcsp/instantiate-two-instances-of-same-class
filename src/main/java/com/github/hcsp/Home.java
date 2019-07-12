5237ff3d4f622dc15a80c0e39dbde46b6bb0938d
frank-0908 <2059326740@qq.com>
2019-07-12 20:19:12 +0800

    创建同一个类的多个对象

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -13,6 +13,12 @@ public class Home {
     }
 
     public static void createTwoCats() {
+        Cat Black=new Cat();
+        Black.name="张三";
+        black=Black;
+        Cat White=new Cat();
+        White.name="李四";
+        white=White;
         // Create two new Cats named "Black" and "White"
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个名字为"Black"和"White"的Cat实例，分别赋值给"black"和"white"静态变量
