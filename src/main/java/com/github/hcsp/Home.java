fbc9b77f528fb544ecd21764c6710b3ddde8721b
frank-0908 <52615871+frank-0908@users.noreply.github.com>
2019-07-12 21:01:51 +0800

    Java程序的基本结构练习 (#44)
    Java程序的基本结构练习 (#44)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -13,6 +13,13 @@ public class Home {
     }
 
     public static void createTwoCats() {
+        Cat Black=new Cat();
+        Black.name="Black";
+        black=Black;
+
+        Cat White=new Cat();
+        White.name="White";
+        white=White;
         // Create two new Cats named "Black" and "White"
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个名字为"Black"和"White"的Cat实例，分别赋值给"black"和"white"静态变量
