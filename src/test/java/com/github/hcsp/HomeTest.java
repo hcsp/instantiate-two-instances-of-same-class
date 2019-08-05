2aabc216f392b8950390b00f203ffc8606bcb3e5
PingAnNetBar <840495823@qq.com>
2019-08-06 00:03:02 +0800

    实例化两个对象并赋值给静态变量

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class HomeTest {
     @Test
     public void testNewCats() {
-       // Home.createTwoCats();
+        Home.createTwoCats();
         Assertions.assertEquals("cat(black)", Home.black.toString().toLowerCase());
         Assertions.assertEquals("cat(white)", Home.white.toString().toLowerCase());
     }
