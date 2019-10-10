8f1e38728bda894c48df09bdade5d5657f5020ac
wangyujie1207 <82414199@qq.com>
2019-10-10 16:30:26 +0800

    new two Cat,write name

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -3,18 +3,22 @@ package com.github.hcsp;
 import com.github.hcsp.pet.Cat;
 
 public class Home {
-    public static Cat black;
-    public static Cat white;
+  public static Cat black;
+  public static Cat white;
 
-    public static void main(String[] args) {
-        createTwoCats();
-        System.out.println(black);
-        System.out.println(white);
-    }
+  public static void main(String[] args) {
+    createTwoCats();
+    System.out.println(black);
+    System.out.println(white);
+  }
 
-    public static void createTwoCats() {
-        // Create two new Cats named "Black" and "White"
-        // then assign them to "black" and "white" static variable respectively
-        // 在这里创建两个Cat实例，分别赋值给"black"和"white"静态变量，然后将它们的"name"成员变量分别设置为"Black"和"White"
-    }
+  public static void createTwoCats() {
+    // Create two new Cats named "Black" and "White"
+    // then assign them to "black" and "white" static variable respectively
+    // 在这里创建两个Cat实例，分别赋值给"black"和"white"静态变量，然后将它们的"name"成员变量分别设置为"Black"和"White"
+    Cat black = new Cat();
+    Cat white = new Cat();
+    black.name = "Black";
+    white.name = "White";
+  }
 }
