49c1deeb4e397c2e2ba543ac8118ff033a1c32c9
chengdasan <chenghhao@foxmail.com>
2019-10-19 20:49:39 +0800

    在这里创建两个Cat实例，分别赋值给"black"和"white"静态变量

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -1,9 +1,7 @@
 package com.github.hcsp;
 
 import com.github.hcsp.pet.Cat;
-import com.sun.jmx.snmp.SnmpUnknownAccContrModelException;
 
-import javax.xml.namespace.QName;
 
 public class Home {
     public static Cat black;
