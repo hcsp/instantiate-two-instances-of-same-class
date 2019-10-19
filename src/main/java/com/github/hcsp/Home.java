cad9cb2359ca677816dc6e6a332e156b6a991185
chengdasan <56035308+chengdasan@users.noreply.github.com>
2019-10-19 21:00:08 +0800

    在这里创建两个Cat实例，分别赋值给"black"和"white"静态变量 (#110)
    在这里创建两个Cat实例，分别赋值给"black"和"white"静态变量 (#110)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -2,6 +2,7 @@ package com.github.hcsp;
 
 import com.github.hcsp.pet.Cat;
 
+
 public class Home {
     public static Cat black;
     public static Cat white;
@@ -16,5 +17,9 @@ public class Home {
         // Create two new Cats named "Black" and "White"
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个Cat实例，分别赋值给"black"和"white"静态变量，然后将它们的"name"成员变量分别设置为"Black"和"White"
+        black = new Cat();
+        black.name = "black";
+        white = new Cat();
+        white.name = "white";
     }
 }
