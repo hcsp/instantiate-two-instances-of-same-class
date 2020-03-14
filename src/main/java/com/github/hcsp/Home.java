7a022e96b5b69ffa7b5e05156052af9eccc14785
Tonny Ge <2454424391@qq.com>
2020-03-14 13:58:16 +0800

    two cat (#190)
    two cat (#190)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -11,10 +11,15 @@ public class Home {
         System.out.println(black);
         System.out.println(white);
     }
+    
 
     public static void createTwoCats() {
         // Create two new Cats named "Black" and "White"
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个Cat实例，分别赋值给"black"和"white"静态变量，然后将它们的"name"成员变量分别设置为"Black"和"White"
+        black = new Cat();
+        white = new Cat();
+        black.name = "Black";
+        white.name = "White";
     }
 }
