b7072b098ffc01d7341003453faffc5aa9500e8e
hue li <49891815+huesf@users.noreply.github.com>
2020-04-09 12:10:42 +0800

    创建俩个实例，分别赋值 (#200)
    创建俩个实例，分别赋值 (#200)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -12,9 +12,16 @@ public class Home {
         System.out.println(white);
     }
 
-    public static void createTwoCats() {
+    public static Cat createTwoCats() {
         // Create two new Cats named "Black" and "White"
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个Cat实例，分别赋值给"black"和"white"静态变量，然后将它们的"name"成员变量分别设置为"Black"和"White"
+        Cat cat = new Cat();
+        black = cat;
+        cat.name="black";
+        Cat cat1 = new Cat();
+        white = cat1;
+        cat1.name ="white";
+        return cat;
     }
 }
