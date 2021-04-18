8f64ae67fa9bc1fb9fa4a04498bbc16a04ec3571
sltap <wuzhenan00@gmail.com>
2021-04-19 05:15:59 +0800

    练习5.4.3

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -16,10 +16,8 @@ public class Home {
         // Create two new Cats named "Black" and "White"
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个Cat实例，分别赋值给"black"和"white"静态变量，然后将它们的"name"成员变量分别设置为"Black"和"White"
-        Cat Black=new Cat();
-        Black=black;
-        Cat White=new Cat();
-        White=white;
+        Cat Black=black;
+        Cat White=white;
         Black.name="Black";
         White.name="White";
     }
