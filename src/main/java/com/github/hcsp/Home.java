80a14aaf9978daddf6a9252d566c3a995c04dc79
jcbound <784538725@qq.com>
2021-11-22 22:11:15 +0800

    创建两个对象实例，并给对应的变量赋值

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -17,8 +17,8 @@ public class Home {
         // then assign them to "black" and "white" static variable respectively
         // 在这里创建两个Cat实例，分别赋值给"black"和"white"静态变量，然后将它们的"name"成员变量分别设置为"Black"和"White"
         Cat black = new Cat();
-        black.name="Black";
+        black.name = "Black";
         Cat white = new Cat();
-        white.name="White";
+        white.name = "White";
     }
 }
